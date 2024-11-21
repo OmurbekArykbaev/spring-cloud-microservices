9fa1b4c3a58b13e980d79edbbda99cb47557e07b
Омурбек Арыкбаев <firefoxer00@gmail.com>
2024-11-21 18:07:06 +0600

    add more files

--- discovery-service/src/main/java/kg/nutelecom/discoveryserver/config/SecurityConfig.java
+++ discovery-service/src/main/java/kg/nutelecom/discoveryserver/config/SecurityConfig.java
@@ -1,46 +1,57 @@
-package kg.nutelecom.discoveryserver.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig  {
-
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder.inMemoryAuthentication()
-                .withUser("eureka").password("password").authorities("USER");
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        return authenticationManagerBuilder
-                .inMemoryAuthentication()
-                .withUser("eureka")
-                .password("password") // Шифруем пароль
-                .authorities("USER")
-                .and()
-                .build();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-
-                .httpBasic();
-
-        return httpSecurity.build();
-    }
-
-}
+//package kg.nutelecom.discoveryserver.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.AuthenticationEntryPoint;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+////@EnableWebSecurity
+//public class SecurityConfig   {
+//
+//
+////    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+////        authenticationManagerBuilder
+////                .inMemoryAuthentication()
+////                .withUser("eureka")
+////                .password("password")
+////                .authorities("USER");
+////    }
+////
+////
+////
+////    @Bean
+////    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+////        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+////                .authorizeHttpRequests(request -> request.requestMatchers(
+////                        "/eureka/**",       // Для маршрутов Eureka
+////                        "/actuator/**"      // (Опционально) для Actuator
+////                ).permitAll().anyRequest().authenticated());
+////
+////
+////        httpSecurity.oauth2ResourceServer((oauth2) -> oauth2
+////                .jwt(Customizer.withDefaults()));
+////
+////        httpSecurity
+////                .httpBasic(Customizer.withDefaults())
+////                .sessionManagement(sessionManagementCustomizer -> sessionManagementCustomizer
+////                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+////
+////
+////        return httpSecurity.build();
+////    }
+////
+//}
+////    @Bean
+////    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+////        return config.getAuthenticationManager();
+////    }
